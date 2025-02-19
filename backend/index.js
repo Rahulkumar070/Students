6b4c993bc65e20e6286dc9ed3aeefa96e4f76803
Rahul kumar Pal <151990777+Rahulkumar070@users.noreply.github.com>
2025-02-19 13:32:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
 mongoose
